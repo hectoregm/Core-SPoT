f5442b0d9389af4411d93031ec21e42bbc33d6e1
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2013-07-15 20:40:48 -0500

    Initial Commit

---
+++ Core-SPoT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Core-SPoT
+//
+//  Created by Hector Enrique Gomez Morales on 7/15/13.
+//  Copyright (c) 2013 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Core-SPoT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Core-SPoT
+//
+//  Created by Hector Enrique Gomez Morales on 7/15/13.
+//  Copyright (c) 2013 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
